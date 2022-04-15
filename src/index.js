f5d1cdfc0d0f5c65cb39abe56c3dcd658c0fe927
Dima Kovalev <dimakov7@gmail.com>
2022-04-15 14:09:11 +0300

    init(task16): upload task 16

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Text from "./Text";
-import Box from "./Box";
+import Task from "./Task";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="container">
-      <Box label="Small box" cssClass="smallBox" />
-      <Box label="Medium Box" cssClass="mediumBox" />
-      <Box label="Big box" cssClass="bigBox" />
-    </div>
+    <Task number={1} text="Написать список" done={true} />
+    <Task number={2} text="Зачеркнуть первый вариант" />
+    <Task number={3} text="Сделать вид, что очень занят работой" />
+    <Task number={4} />
+    <Task number={5} text="Придумать интересный пункт 4" />
   </React.StrictMode>
 );
