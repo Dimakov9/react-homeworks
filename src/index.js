7484a4ab2942a3c87c28f9c3940a364965c4cda0
Dima Kovalev <dimakov7@gmail.com>
2022-04-22 12:36:30 +0300

    init(task8): upload task8

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Task from "./Task";
+import ItalicText from "./ItalicText";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Task number={1} text="Написать список" done={true} />
-    <Task number={2} text="Зачеркнуть первый вариант" />
-    <Task number={3} text="Сделать вид, что очень занят работой" />
-    <Task number={4} />
-    <Task number={5} text="Придумать интересный пункт 4" />
+    <ItalicText />
   </React.StrictMode>
 );
