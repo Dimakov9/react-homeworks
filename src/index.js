3f779f8b1e26e9d3ab058c3830ce34485fa65f88
Dima Kovalev <dimakov7@gmail.com>
2022-04-27 17:24:00 +0300

    init(task9) upload task9

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CatGame from "./CatGame";
+
 import "./index.css";
-import Task from "./Task";
+// import Task from "./Task";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Task number={1} text="Написать список" done={true} />
-    <Task number={2} text="Зачеркнуть первый вариант" />
-    <Task number={3} text="Сделать вид, что очень занят работой" />
-    <Task number={4} />
-    <Task number={5} text="Придумать интересный пункт 4" />
+    <CatGame />
   </React.StrictMode>
 );
