37bd75a5cf3624305dc791d4e3f4f2e680039475
Dima Kovalev <dimakov7@gmail.com>
2022-05-06 18:35:59 +0300

    fix(task17) fixed notes in commentst

--- src/SayHi.jsx
+++ src/SayHi.jsx
@@ -5,22 +5,20 @@ function SayHi() {
   const [loading, setLoading] = useState(false);
   const [greeting, setGreeting] = useState("");
 
-  const isLoading = () => {
+  const logIn = () => {
     setTimeout(sayHi, 3000);
     setLoading(true);
     setGreeting(""); //сделал чтоб пропадала надпись Hello когда ещё раз нажимаем на кнопку ))
-    console.log(loading);
   };
 
   const sayHi = () => {
     setGreeting("Hello!!!");
     setLoading(false);
-    console.log(greeting);
   };
 
   return (
     <div className="container">
-      <button onClick={isLoading}>Say Hi!</button>
+      <button onClick={logIn}>Say Hi!</button>
       {loading && <div className="loader" />}
 
       <span>{greeting}</span>
